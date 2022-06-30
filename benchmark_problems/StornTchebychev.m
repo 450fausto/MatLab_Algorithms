1355a0427ec272af3edb7aea0683a8f602352816
450Fausto <74079377+450fausto@users.noreply.github.com>
2022-06-30 16:56:11 -0500

    Rename StornTchebychev.m to benchmark_problems/StornTchebychev.m

--- benchmark_problems/StornTchebychev.m
+++ benchmark_problems/StornTchebychev.m
@@ -1,8 +1,8 @@
 function f=StornTchebychev(x)                                        
-% Storn’s Tchebychev Problem (ST) n=9, 17                            |
+% Stornâ€™s Tchebychev Problem (ST) n=9, 17                            |
 % LimInf=[-128 -128 ... -128]; LimSup=[128 128 ... 128];             |
 % Fojmin=0                                                          \|/ 
-% x*=[128 0 -256 0 160 0 -32 0,1]  la dimensión de este vector es 16 V
+% x*=[128 0 -256 0 160 0 -32 0,1]  la dimensiÃ³n de este vector es 16 V
 % [32768 0 -1331072 0 21299 0 -180224 84480 0 -2154 0 2688 0 -128 0 1]
 n=length(x); m=60*(n==9)+100*(n==17); k=0; 
 d=72.661*(n==9)+10558.145*(n==17);
