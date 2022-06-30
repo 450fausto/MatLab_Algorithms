831a359eb6ed79267430ba0e8d7e7121967983e7
450Fausto <74079377+450fausto@users.noreply.github.com>
2022-06-30 15:59:05 -0500

    Add files via upload

---
+++ Aluffi_Pentini.m
@@ -0,0 +1,8 @@
+function y=Aluffi_Pentini(x)
+% Aluffi-Pentini’s Problem (AP) n=2
+% The number of local minima is two
+% LimInf=[-10 -10]; LimSup=[10 10];
+% Fojmin~= -0.3523...
+% x*=[-1.0465 0];
+y=0.25*x(1)^4-0.5*x(1)^2+0.1*x(1)+0.5*x(2)^2;
+end
